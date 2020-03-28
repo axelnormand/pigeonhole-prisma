8294002becfc597f10f63c3fb11c2dd426050f77
axelnormand <axel@axelnormand.co.uk>
2020-03-28 23:44:34 +0000

    nexus gen

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@axelnormand/pigeonhole-prisma";
+import { prisma } from "@axelnormand/pigeonhole-prisma-client";
 
 // A `main` function so that we can use async/await
 async function main() {
