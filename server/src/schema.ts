775f9ddf206567b16b5673f8491cc97c93cf7d2f
axelnormand <axel@axelnormand.co.uk>
2020-04-01 23:30:35 +0100

    using auth example

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,66 +1,25 @@
-import { prisma } from "@axelnormand/pigeonhole-prisma-client";
-import datamodelInfo from "@axelnormand/pigeonhole-nexus-client";
-import * as path from "path";
-import { stringArg, idArg } from "nexus";
-import { prismaObjectType, makePrismaSchema } from "nexus-prisma";
+import { nexusPrismaPlugin } from "nexus-prisma";
+import { makeSchema } from "nexus";
+import * as types from "./types";
 
-const Query = prismaObjectType<"Query">({
-  name: "Query",
-  definition(t) {
-    // t.prismaFields(["post"]);
-    // t.list.field("feed", {
-    //   type: "Post",
-    //   resolve: (_, args, ctx) =>
-    //     ctx.prisma.posts({ where: { published: true } }),
-    // });
-    // t.list.field("postsByUser", {
-    //   type: "Post",
-    //   args: { email: stringArg() },
-    //   resolve: (_, { email }, ctx) =>
-    //     ctx.prisma.posts({ where: { author: { email } } }),
-    // });
-  },
-});
-
-const Mutation = prismaObjectType<"Mutation">({
-  name: "Mutation",
-  definition(t) {
-    // t.prismaFields(["createUser", "deletePost"]);
-    // t.field("createDraft", {
-    //   type: "Post",
-    //   args: {
-    //     title: stringArg(),
-    //     authorId: idArg({ nullable: true })
-    //   },
-    //   resolve: (_, { title, authorId }, ctx) =>
-    //     ctx.prisma.createPost({
-    //       title,
-    //       author: { connect: { id: authorId } }
-    //     })
-    // });
-    // t.field("publish", {
-    //   type: "Post",
-    //   nullable: true,
-    //   args: { id: idArg() },
-    //   resolve: (_, { id }, ctx) =>
-    //     ctx.prisma.updatePost({
-    //       where: { id },
-    //       data: { published: true }
-    //     })
-    // });
-  },
-});
-
-export const schema = makePrismaSchema({
-  types: [Query, Mutation],
-
-  prisma: {
-    datamodelInfo,
-    client: prisma,
-  },
-
+export const schema = makeSchema({
+  types,
+  plugins: [nexusPrismaPlugin()],
   outputs: {
-    schema: path.join(__dirname, "./generated/schema.graphql"),
-    typegen: path.join(__dirname, "./generated/nexus.ts"),
+    schema: __dirname + "/../../schema.graphql",
+    typegen: __dirname + "/generated/nexus.ts",
+  },
+  typegenAutoConfig: {
+    sources: [
+      {
+        source: "@prisma/client",
+        alias: "client",
+      },
+      {
+        source: require.resolve("./context"),
+        alias: "Context",
+      },
+    ],
+    contextType: "Context.Context",
   },
 });
