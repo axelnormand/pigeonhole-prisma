0556549eaf53035554508f8b237282b34a9914c1
axelnormand <axel@axelnormand.co.uk>
2020-04-09 21:45:06 +0100

    shouldGenerateArtifacts: false

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,7 +4,7 @@ import * as types from './types';
 
 export const schema = makeSchema({
   types,
-  plugins: [nexusPrismaPlugin()],
+  plugins: [nexusPrismaPlugin({ shouldGenerateArtifacts: false })],
   outputs: {
     schema: __dirname + '/../../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
