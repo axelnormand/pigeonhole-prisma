eb001b7ab4117ec5a01e15e6afc90578171c00d9
axelnormand <axel@axelnormand.co.uk>
2020-04-13 21:42:17 +0100

    trying process.cwd not dirname

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,20 +1,36 @@
+import path from 'path';
 import { nexusPrismaPlugin } from 'nexus-prisma';
 import { makeSchema } from 'nexus';
 import * as types from './types';
+
+/**
+ * __dirname not working on netlify
+ * Error: ENOENT: no such file or directory, mkdir
+ *
+ * Trying solution from here instead
+ * https://github.com/prisma/prisma/issues/1021
+ */
+const getPathFromDirname = (...fileParts: string[]) => {
+  // process.cwd is the server dir, so add on src
+  return path.join(process.cwd(), 'src', ...fileParts);
+};
 
 export const schema = makeSchema({
   types,
   plugins: [
     nexusPrismaPlugin({
       outputs: {
-        typegen: __dirname + '/generated/nexusTypes.ts',
+        //typegen: __dirname + '/generated/nexusTypes.ts',
+        typegen: getPathFromDirname('generated', 'nexusTypes.ts'),
       },
       shouldGenerateArtifacts: false,
     }),
   ],
   outputs: {
-    schema: __dirname + '/../../schema.graphql',
-    typegen: __dirname + '/generated/nexus.ts',
+    //schema: __dirname + '/../../schema.graphql',
+    schema: getPathFromDirname('../../', 'schema.graphql'),
+    //typegen: __dirname + '/generated/nexus.ts',
+    typegen: getPathFromDirname('generated', 'nexus.ts'),
   },
   typegenAutoConfig: {
     sources: [
