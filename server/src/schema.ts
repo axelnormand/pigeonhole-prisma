b995d33d2e8a50f5925d2c014e4d7d7959656b5e
axelnormand <axel@axelnormand.co.uk>
2020-04-13 22:01:00 +0100

    trying not schema outputs on netlify

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -1,37 +1,28 @@
-import path from 'path';
 import { nexusPrismaPlugin } from 'nexus-prisma';
 import { makeSchema } from 'nexus';
 import * as types from './types';
+import { config } from './config';
 
-/**
- * __dirname not working on netlify
- * Error: ENOENT: no such file or directory, mkdir
- *
- * Trying solution from here instead to get generated dir path
- * https://github.com/prisma/prisma/issues/1021
- */
-const getGeneratedFilePath = (filename: string) => {
-  // process.cwd is the server dir, so add on src
-  return path.join(process.cwd(), 'src', 'generated', filename);
-};
+//__dirname not working on netlify
+// Error: ENOENT: no such file or directory, mkdir
+// Trying not generating nexus types on netlify
+// Therefore schema outputs undefined for netlify
 
 export const schema = makeSchema({
   types,
   plugins: [
     nexusPrismaPlugin({
-      outputs: {
-        //typegen: __dirname + '/generated/nexusTypes.ts',
-        typegen: getGeneratedFilePath('nexusTypes.ts'),
-      },
-      shouldGenerateArtifacts: false,
+      shouldGenerateArtifacts:
+        config().pigeonholeServer === 'node' ? true : false,
     }),
   ],
-  outputs: {
-    //schema: __dirname + '/../../schema.graphql',
-    schema: getGeneratedFilePath('schema.graphql'),
-    //typegen: __dirname + '/generated/nexus.ts',
-    typegen: getGeneratedFilePath('nexus.ts'),
-  },
+  outputs:
+    config().pigeonholeServer === 'node'
+      ? {
+          schema: __dirname + '/../../schema.graphql',
+          typegen: __dirname + '/generated/nexus.ts',
+        }
+      : undefined,
   typegenAutoConfig: {
     sources: [
       {
