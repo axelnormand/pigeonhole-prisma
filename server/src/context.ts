bd7d7e3b945b6d40d2027def39d1cb0372f5d64c
axelnormand <axel@axelnormand.co.uk>
2020-04-16 23:23:41 +0100

    typed request

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
 import { ContextParameters } from 'graphql-yoga/dist/types';
+import { Request } from 'express';
 
 const prisma = new PrismaClient();
 
 export interface Context {
   prisma: PrismaClient;
-  request: any;
+  request: Request;
 }
 
 export const createContext = (request: ContextParameters) => ({
