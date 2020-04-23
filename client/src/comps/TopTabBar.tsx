9f49f9c0d1fc82ea0b19965d176db0079aca106f
axelnormand <axel@axelnormand.co.uk>
2020-04-23 23:20:41 +0100

    safe area top tabs

--- client/src/comps/TopTabBar.tsx
+++ client/src/comps/TopTabBar.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
-import { TabBar, Tab } from '@ui-kitten/components';
+import { TabBar, Tab, IconProps } from '@ui-kitten/components';
 import { MaterialTopTabBarProps } from '@react-navigation/material-top-tabs';
+import { useSafeArea } from 'react-native-safe-area-context';
 
 export const TopTabBar: React.FC<MaterialTopTabBarProps> = ({
   navigation,
   state,
   descriptors,
-}) => (
-  <TabBar
-    selectedIndex={state.index}
-    onSelect={(index) => navigation.navigate(state.routeNames[index])}
-  >
-    {state.routes.map((route) => {
-      const { options } = descriptors[route.key];
-      return (
-        <Tab key={route.key} title={options.title} icon={options.tabBarIcon} />
-      );
-    })}
-  </TabBar>
-);
+}) => {
+  const insets = useSafeArea();
+  return (
+    <TabBar
+      selectedIndex={state.index}
+      onSelect={(index) => navigation.navigate(state.routeNames[index])}
+      style={{ paddingTop: insets.top }}
+    >
+      {state.routes.map((route) => {
+        const { options } = descriptors[route.key];
+        return (
+          <Tab
+            key={route.key}
+            title={options.title}
+            icon={options.tabBarIcon as IconProps}
+          />
+        );
+      })}
+    </TabBar>
+  );
+};
