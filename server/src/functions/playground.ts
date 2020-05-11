0e6783ef74ffe65d11ff81cf01c558371b327bf8
axelnormand <axel@axelnormand.co.uk>
2020-05-11 20:15:47 +0100

    callbackWaitsForEmptyEventLoop fix

--- server/src/functions/playground.ts
+++ server/src/functions/playground.ts
@@ -3,4 +3,13 @@ import { getNetlify } from '../server';
 console.log(`🚀 Starting lambda graphql playground`);
 const lambda = getNetlify();
 
-export const handler = lambda.playgroundHandler;
+// Netlify was always timing out: https://github.com/prisma/prisma/issues/2241
+export const handler = (
+  event: any,
+  context: { callbackWaitsForEmptyEventLoop: boolean },
+  cb: any,
+) => {
+  // Set to false to send the response right away when the callback executes, instead of waiting for the Node.js event loop to be empty.
+  context.callbackWaitsForEmptyEventLoop = false;
+  return lambda.graphqlHandler(event, context, cb);
+};
