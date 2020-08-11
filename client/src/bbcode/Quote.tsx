71330bee06de01b01c52991f2b26d758237014b1
axelnormand <axel@axelnormand.co.uk>
2020-08-11 23:17:14 +0100

    quote flex n bg

--- client/src/bbcode/Quote.tsx
+++ client/src/bbcode/Quote.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import { Text, Layout } from '@ui-kitten/components';
+import { Text } from '@ui-kitten/components';
 import { Italic } from './Italic';
 import { dark } from '@eva-design/eva';
 
@@ -9,11 +9,17 @@ export const Quote: React.FC<{ name?: string; children: string }> = ({
   children,
 }) => {
   const border = dark['color-basic-500'];
+  const background = dark['color-basic-900'];
   return (
-    <Layout style={[styles.container, { borderColor: border }]}>
+    <View
+      style={[
+        styles.container,
+        { borderColor: border, backgroundColor: background },
+      ]}
+    >
       {name && <Italic testID="bbcode-quote-name">{name} wrote:</Italic>}
       <Text testID="bbcode-quote-text">{children}</Text>
-    </Layout>
+    </View>
   );
 };
 
@@ -22,5 +28,7 @@ const styles = StyleSheet.create({
     padding: 16,
     borderWidth: 2,
     borderRadius: 8,
+    flex: 1,
+    display: 'flex',
   },
 });
