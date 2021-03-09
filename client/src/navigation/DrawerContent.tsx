3699218812c1ca5de09b17dcf700116144d8d5ef
Alex Norman <alex@upsidetechnology.co>
2021-03-09 23:27:42 +0000

    linting

--- client/src/navigation/DrawerContent.tsx
+++ client/src/navigation/DrawerContent.tsx
@@ -1,8 +1,13 @@
-import React, { useCallback, useContext } from 'react';
-import { Drawer as KittenDrawer, IndexPath, Layout, DrawerItem } from '@ui-kitten/components';
-import { DrawerContentComponentProps } from '@react-navigation/drawer';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { StoreContext } from '../models';
+import React, { useCallback, useContext } from "react";
+import {
+  Drawer as KittenDrawer,
+  IndexPath,
+  Layout,
+  DrawerItem,
+} from "@ui-kitten/components";
+import { DrawerContentComponentProps } from "@react-navigation/drawer";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { StoreContext } from "../models";
 
 export const DrawerContent: React.FC<DrawerContentComponentProps> = ({
   navigation,
@@ -10,22 +15,23 @@ export const DrawerContent: React.FC<DrawerContentComponentProps> = ({
 }) => {
   const insets = useSafeAreaInsets();
   const store = useContext(StoreContext);
-  const onSelect = useCallback( (index: IndexPath) => {
+  const onSelect = useCallback((index: IndexPath) => {
     if (index.row !== 1) {
       navigation.navigate(state.routeNames[index.row]);
     } else if (index.row === 1) {
       store.logout();
     }
-  },[]);
+  }, []);
 
   return (
     <Layout style={{ paddingTop: insets.top, flex: 1 }}>
       <KittenDrawer
         selectedIndex={new IndexPath(state.index)}
-        onSelect={onSelect}>
-        <DrawerItem title='Home' />
-        <DrawerItem title='Logout' />
-        </KittenDrawer>
+        onSelect={onSelect}
+      >
+        <DrawerItem title="Home" />
+        <DrawerItem title="Logout" />
+      </KittenDrawer>
     </Layout>
   );
 };
